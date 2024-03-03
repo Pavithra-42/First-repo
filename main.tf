f625e556d42b52b6439e9aa7f72375b19a3518a3
Pavithra-42 <157984965+Pavithra-42@users.noreply.github.com>
2024-03-03 15:11:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,22 +10,16 @@ variable "vpc_id" {
   type        = string
 }
 
-# Generate a new key pair
-resource "aws_key_pair" "example_key_pair" {
-  key_name   = "example-key-pair"
-  public_key = file("C:/Users/Hi/.ssh/id_rsa.pub")
-# Replace with the path to your public key
-}
-
 # Create an EC2 instance within the existing VPC
 resource "aws_instance" "example_instance" {
   ami           = "ami-07d9b9ddc6cd8dd30"  # Replace with your desired AMI ID
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.example_key_pair.key_name
-  vpc_security_group_ids = ["sg-0a9fc10324fe8e760"]  # Replace with the ID of your existing security group
+  key_name      = "example-key-pair"  # This is the name of the key pair to be created by AWS
+
   subnet_id     = "subnet-071e013c3e6aebd3e"  # Replace with the ID of your existing subnet
 
   tags = {
     Name = "example-instance"
   }
 }
+
